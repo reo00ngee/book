ea047d2cc8a68b889362c09525554a60e68bf868
reo00ngee <reo00ngee@gmail.com>
2023-03-23 01:24:29 +0900

    commit

--- part2/src/index.php
+++ part2/src/index.php
@@ -1,6 +1,23 @@
 <?php
+require_once __DIR__ . '/mysqli.php';
+
+
+
+function listReviews($link)
+{
+  $book_logs = [];
+  $sql = 'SELECT title,author,status,assessment,impression FROM book_log;';
+  $result = mysqli_query($link, $sql);
+  while ($book_log = mysqli_fetch_assoc($result)) {
+    $book_logs[] = $book_log;
+  }
+  mysqli_free_result($result);
+  return $book_logs;
+}
+
+$link = dbConnect();
+$book_logs = listReviews($link);
 
 $title = '読書ログの一覧';
 $content = __DIR__ . '/views/index.php';
 include __DIR__ . '/views/layout.php';
-
--- part2/src/views/index.php
+++ part2/src/views/index.php
@@ -1,11 +1,29 @@
-
-  <h1>会社情報の一覧</h1>
-<a href ="new.php">会社情報を登録する</a>
+<h1>会社情報の一覧</h1>
+<a href="new.php">会社情報を登録する</a>
 <main>
+  <?php if (count($book_logs) > 0) : ?>
+  <?php foreach ($book_logs as $book_log) : ?>
+
   <section>
-    <h2>saaaaa</h2>
-    <div>著者名</div>
+    <h2>
+      <?php echo $book_log['title'] ?>
+    </h2>
+    <div>著者名：
+      <?php echo $book_log['author'] ?>
+    </div>
+    <div>読書状況：
+      <?php echo $book_log['status'] ?>
+    </div>
+    <div>評価：
+      <?php echo $book_log['assessment'] ?>
+    </div>
+    <div>感想：
+      <?php echo $book_log['impression'] ?>
+    </div>
   </section>
+  <?php endforeach; ?>
+  
+  <?php else : ?>
+    <p>読書ログが登録されていません。</p>
+  <?php endif; ?>
 </main>
-
-
